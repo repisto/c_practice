9ed1f4e5d8f54036d7cd6a3aec7fe6457897b6ae
repisto <tony0329taehoon@gmail.com>
2025-07-22 16:55:18 +0900

    Add files

---
+++ C1/Project1/build/CMakeFiles/my_program.dir/src/main.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/my_program.dir/src/main.c.o: \
+  /Users/taehoonyoon/Projects/Modoo/C1/Project1/src/main.c \
+  /Users/taehoonyoon/Projects/Modoo/C1/Project1/include/hello.h
---
+++ C1/Project1/src/hello.c
@@ -0,0 +1,4 @@
+#include <hello.h>
+#include <stdio.h>
+
+void hello() { printf("Hello, World!\n"); }
